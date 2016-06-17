a5d50eddce68ed8f562d929e02167e21f812b903
suzee <2469209357@qq.com>
2016-06-17 20:35:07 +0800

    Initial Commit

---
+++ NeteaseNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeteaseNews
+//
+//  Created by fanzz on 16/6/17.
+//  Copyright © 2016年 fanzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
